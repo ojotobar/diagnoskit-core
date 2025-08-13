b70bad58fb87116cdb575bb64e855987de023925
Toba Ojo <ojotobar@gmail.com>
2025-08-13 12:25:38 +0100

    first commit

---
+++ src/DiagnosKit.Core/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DiagnosKit.Core.Logging.Contracts;
+using DiagnosKit.Core.Logging.Implementations;
+using DiagnosKit.Core.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiagnosKit.Core.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void UseUnifiedErrorHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<UnifiedErrorHandlerMIddleware>();
+        }
+
+        public static void AddLoggerManager(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
---
+++ src/DiagnosKit.Core/Logging/Contracts/ILoggerManager.cs
@@ -0,0 +1,10 @@
+﻿namespace DiagnosKit.Core.Logging.Contracts
+{
+    public interface ILoggerManager
+    {
+        void LogDebug(string message);
+        void LogError(string message);
+        void LogInfo(string message);
+        void LogWarn(string message);
+    }
+}
